48c50c9d7df1ad70132f1b05abcdbd46f2c45698
nyainanathan <nyaina.nathan.1@gmail.com>
2025-08-08 17:44:46 +0300

    feat: user can edit characters

--- front-end/src/Components/MainTable.jsx
+++ front-end/src/Components/MainTable.jsx
@@ -1,24 +1,30 @@
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 const MainTable = () => {
     
     const [characters, setCharacters] = useState([]);
     const PORT = 3000;
     const baseUrl = "http://127.0.0.1:" + PORT;
+    const [operations, setOperations] = useState(0);
 
     const [id, setId] = useState("ID");
     const [name, setName] = useState("");
     const [realName, setRealName] = useState("");
     const [universe, setUniverse] = useState("");
 
+    const nameRef = useRef(null);
+    const realNameRef = useRef(null);
+    const universeRef = useRef(null);
+
+    const fetchData = async () => {
+        const response = await axios.get(baseUrl + "/characters");
+        setCharacters(response.data);
+    };
+
     useEffect(() => {
-        const fetchData = async () => {
-            const response = await axios.get(baseUrl + "/characters");
-            setCharacters(response.data);
-        };
         fetchData();
-    }, []);
+    }, [operations] );
 
 
     const handleEdit = (characterId) => {
@@ -44,6 +50,24 @@ const MainTable = () => {
         setUniverse("");
     }
 
+    const handleSubmit = async () => {
+        const editedCharacter = { id, name, realName, universe };
+        console.log(editedCharacter);
+       
+        await fetch(`${baseUrl}/characters/${id}`, {
+            method: "PUT",
+            headers:{
+                "Content-type" : "application/json"
+            },
+            body: JSON.stringify(editedCharacter)
+        } );
+
+        resetInputs();
+
+        setOperations(r => r+1)
+        
+    }
+
     return (
         <>
             <h1 className='text-center p-4 text-4xl'>MCU Characters list</h1>
@@ -56,7 +80,7 @@ const MainTable = () => {
                     <th>Universe</th>
                     <th>Actions</th>
                 </tr>
-                {characters.map((character, index) => (
+                {characters.map((character) => (
                     <tr key={character.id} className='h-12 text-center border-1'>
                         <td className='w-1/5'>{character.id}</td>
                         <td className='w-1/5'>{character.name}</td>
@@ -74,6 +98,7 @@ const MainTable = () => {
                     </td>
                     <td>
                         <input 
+                        ref={nameRef}
                         className='text-center outline-0' 
                         type="text" 
                         name="name" 
@@ -81,10 +106,12 @@ const MainTable = () => {
                         placeholder='Name...' 
                         value={name}
                         onChange={(e) => setName(e.target.value)}
+                        required
                         />
                     </td>
                     <td>
                         <input 
+                        ref={realNameRef}
                         className='text-center outline-0' 
                         type="text" 
                         name="realName" 
@@ -92,20 +119,23 @@ const MainTable = () => {
                         placeholder='Real Name...'
                         value={realName}
                         onChange={(e) => setRealName(e.target.value)}
+                        required
                         />
                     </td>
                     <td>
                         <input 
+                        ref={universeRef}
                         className='text-center outline-0' 
                         type="text" name='universe' 
                         id='universe' 
                         placeholder='Universe ...'
                         value={universe}
                         onChange={(e) => setUniverse(e.target.value)}
+                        required
                         />
                     </td>
                     <td> 
-                        <button className='mr-4'>Confirm</button>
+                        <button onClick={handleSubmit} className='mr-4'>Confirm</button>
                         <button onClick={resetInputs}>Clear</button>
                     </td>
                 </tr>
